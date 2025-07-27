692f8b24289e80a845cd7f39bef3851840d5d013
Higor Code <higorscontato@gmail.com>
2025-07-26 22:29:37 -0300

    feat: add initial server setup to start

---
+++ src/config/plugins/swagger.ts
@@ -0,0 +1,25 @@
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
+import type { FastifyInstance } from 'fastify'
+import { jsonSchemaTransform } from 'fastify-type-provider-zod'
+
+export const registerSwagger = (app: FastifyInstance) => {
+  app.register(fastifySwagger, {
+    openapi: {
+      info: {
+        title: 'SaaS Finance API',
+        description: 'API documentation for SaaS Finance application',
+        version: '1.0.0',
+      },
+    },
+    transform: jsonSchemaTransform,
+  })
+
+  app.register(fastifySwaggerUi, {
+    routePrefix: '/docs',
+    uiConfig: {
+      docExpansion: 'list',
+      deepLinking: false,
+    },
+  })
+}
