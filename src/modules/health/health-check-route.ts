e9d793e44abe6a79a7a02f82c5ab3263181992cb
Higor Code <higorscontato@gmail.com>
2025-07-29 18:13:02 -0300

    fix: remove unused files

--- src/modules/health/health-check-route.ts
+++
@@ -1,7 +0,0 @@
-import type { FastifyInstance } from 'fastify'
-import * as controller from './health-check-controller'
-import * as schema from './health-check-schema'
-
-export const registerHealthRoutes = async (app: FastifyInstance) => {
-  app.get('/health', schema.health, controller.health)
-}
