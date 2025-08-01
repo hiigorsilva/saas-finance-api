7d93b63a8086d8039576f9cfa2b7f6520e27162b
Higor Code <higorscontato@gmail.com>
2025-08-01 00:11:40 -0300

    feat: register workspace route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,10 +3,12 @@ import { signinRoute } from './auth/signin-route'
 import { signupRoute } from './auth/signup-route'
 import { healthRoute } from './check/health-route'
 import { meRoute } from './user/me-route'
+import { workspaceRoute } from './workspace/workspace-route'
 
 export const registerRoutes = (app: FastifyInstance) => {
   app.register(healthRoute, { prefix: '/api' })
   app.register(signupRoute, { prefix: '/api' })
   app.register(signinRoute, { prefix: '/api' })
   app.register(meRoute, { prefix: '/api' })
+  app.register(workspaceRoute, { prefix: '/api' })
 }
