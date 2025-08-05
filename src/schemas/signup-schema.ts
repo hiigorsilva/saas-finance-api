269b4fcdc7a7a5bbdfcac04275e8c34371438f96
Higor Code <higorscontato@gmail.com>
2025-08-05 18:23:55 -0300

    feat: returns a password validation error message

--- src/schemas/signup-schema.ts
+++ src/schemas/signup-schema.ts
@@ -4,7 +4,10 @@ import z from 'zod'
 export const signupBodySchema = z.object({
   name: z.string().trim(),
   email: z.email().trim(),
-  password: z.string().min(8).trim(),
+  password: z
+    .string()
+    .min(8, 'Password must be at least 8 characters long.')
+    .trim(),
 })
 export type SignUpBodyType = z.infer<typeof signupBodySchema>
 
--- src/services/signup-service.ts
+++ src/services/signup-service.ts
@@ -6,6 +6,9 @@ export class SignUpService {
   constructor(private userRepository: UserRepository) {}
 
   async execute(userData: SignUpBodyType) {
+    if (userData.password.length < 8)
+      throw new Error('Password must be at least 8 characters long.')
+
     const userAlreadyExists = await this.userRepository.isUserExistsByEmail(
       userData.email
     )
