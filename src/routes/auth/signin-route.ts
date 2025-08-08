79fff9d323c40e98d63b147747005f781975cfdb
Higor Code <higorscontato@gmail.com>
2025-08-08 14:51:20 -0300

    refactor: organize signin files

--- src/routes/auth/signin-route.ts
+++ src/routes/auth/signin-route.ts
@@ -1,7 +1,7 @@
 import type { FastifyInstance } from 'fastify'
 import { SignInController } from '../../controllers/signin-controller'
 import { UserRepository } from '../../repositories/user-repository'
-import * as schema from '../../schemas/signin-schema'
+import { signinSchema } from '../../schemas/auth/signin-schema'
 import { SignInService } from '../../services/signin-service'
 import { badRequest, internalServerError } from '../../shared/utils/http'
 import { parseResponse } from '../../shared/utils/parse-response'
@@ -11,7 +11,7 @@ const signinService = new SignInService(userRepository)
 const signinController = new SignInController(signinService)
 
 export const signinRoute = (app: FastifyInstance) => {
-  app.post('/signin', schema.signin, async (request, reply) => {
+  app.post('/signin', signinSchema, async (request, reply) => {
     try {
       const response = await signinController.handle(request)
       return reply.status(response.statusCode).send(response)
