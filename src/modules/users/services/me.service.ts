5237d94e3ed69c0a8f82feb61ee187bcc9de568e
Higor Code <higorscontato@gmail.com>
2025-08-26 18:25:23 -0300

    test: add test coverage for getUserData use cases

--- src/modules/users/services/me.service.ts
+++ src/modules/users/services/me.service.ts
@@ -4,7 +4,7 @@ export class MeService {
   constructor(private userRepository: UserRepository) {}
 
   async getUserData(userId: string) {
-    const userAlreadyExists = this.userRepository.isUserExistsById(userId)
+    const userAlreadyExists = await this.userRepository.isUserExistsById(userId)
     if (!userAlreadyExists) throw new Error('User not found')
 
     const user = await this.userRepository.findUserById(userId)
