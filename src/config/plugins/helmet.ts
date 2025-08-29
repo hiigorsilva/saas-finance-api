049be0e4e2efcad006fcadc6d639e2b810f189dc
Higor Code <higorscontato@gmail.com>
2025-08-29 17:55:42 -0300

    chore: add helmet config to handle script, fonts, style, object, frame

--- src/config/plugins/helmet.ts
+++ src/config/plugins/helmet.ts
@@ -4,6 +4,22 @@ import type { FastifyInstance } from 'fastify'
 export const registerHelmet = (app: FastifyInstance) => {
   app.register(helmet, {
     global: true,
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: [
+          "'self'",
+          "'sha256-dYHDKDXG9jWhowwd+1OPBg5K4WQbycudNU9laD/3GPY='",
+        ],
+        styleSrc: ["'self'", "'unsafe-inline'", 'https:'],
+        imgSrc: ["'self'", 'data:', 'https:'],
+        fontSrc: ["'self'", 'https:', 'data:'],
+        objectSrc: ["'none'"],
+        connectSrc: ["'self'"],
+        frameSrc: ["'self'"],
+      },
+    },
     crossOriginEmbedderPolicy: false,
+    crossOriginResourcePolicy: { policy: 'same-site' },
   })
 }
